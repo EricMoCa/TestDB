bfc6773d320becc02e7424923dce2f4dbcac0840
EricMoCa <eric.evmc@gmail.com>
2022-06-04 12:23:34 -0500

    lel2

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 var connection = mysql.createConnection({
-	host: "en1ehf30yom7txe7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+	host: "en1ehf30yom7txe7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: "rkam7mm4feuw6hwh",
     password: "gygqvlres0b1zn2b",
     database : "vj3mza8hgyuw216b"
